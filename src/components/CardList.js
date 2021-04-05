b13d0aa7f61bec4f54253a90f173a1465501dc8f
unknown <ykamakya@yahoo.com>
2021-04-05 11:10:35 -0400

    fixing security issues

---
+++ src/components/CardList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+	if (true) {
+		throw new Error('NOOOO!');
+	}
+	const cardComponent = robots.map((user, i) => {
+	  return (
+	  	<Card
+	  	  key = {i} 
+	  	  id={robots[i].id}
+	  	  name={robots[i].name}
+	  	  email={robots[i].email}
+	  	  />
+      );
+
+	})
+	return(
+		<div>
+		  {cardComponent}
+		</div>
+	);
+}
+
+export default CardList;
